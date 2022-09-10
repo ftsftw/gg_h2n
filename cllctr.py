3b0e3787142658f04f911b83a1778b307aa7621e
ftwftw <feeltheskill@gmail.com>
2022-09-11 01:00:09 +0800

    remove txt files

--- cllctr.py
+++ cllctr.py
@@ -2,6 +2,7 @@ import glob
 
 # find txt files
 parse_files = glob.glob("*.txt")
+#create output txt file and parse all txt into it
 with open("testglob.txt", "w") as outfile: # create output file named result.txt
     for x in parse_files:
         with open(x, "rb") as infile:
