025bca2c0ae91d021c1d140fbfeb1c68aaa923d0
ftwftw <feeltheskill@gmail.com>
2022-09-12 20:02:06 +0800

    strct

--- main.py
+++ main.py
@@ -1,18 +1,1 @@
-nfile = input('Enter file: ')
-
-# try:
-#     tfile = open(nfile)
-# except:
-#     print("Can't find file", nfile)
-#     quit()
-
-import fileinput
-
-with fileinput.FileInput(nfile, inplace=True, backup='.bak') as file:
-
-    for line in file:
-        if 'Dealt' not in line:
-            print(line.replace('Poker Hand #RC', 'PokerStars Hand #20'), end='')
-        else:
-            if '[' in line:
-                print(line.replace('Poker Hand #RC', 'PokerStars Hand #20'), end='')
+# main
